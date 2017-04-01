1d32d20788c202f0ca1153933e30f0171a48e1ab
kevin.leptons@gmail.com <kevin.leptons@gmail.com>
2017-04-01 16:13:07 +0700

    update language rules

---
+++ src/sample/src/sample/squared-num-good-strings.c
@@ -0,0 +1,40 @@
+/*
+DESCRIPTION
+
+    Read a number called x from stdin, then print x^2
+
+AUTHOR
+
+    kevin leptons <kevin.leptons@gmail.com>
+*/
+
+#include <errno.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <cstyle/error.h>
+
+int main(int argc, char *argv[])
+{
+    int x;
+    int res;
+
+    if (argc != 1)
+        // Not accepted any arguments
+        exit_error("不接受任何論據\n");
+
+    // This is program which read a number called x then print x^2
+    printf("這是一個程序，讀取一個名為x的數字，然後打印x ^ 2\n");
+    // Type x:
+    printf("鍵入x: ");
+    res = scanf("%i", &x);
+    if (res == 0)
+        // Not a number
+        exit_error("不是數字");
+    if (res == EOF)
+        exit_error(strerror(errno));
+
+    printf("x^2 = %i\n", x * x);
+
+    return EXIT_SUCCESS;
+}
